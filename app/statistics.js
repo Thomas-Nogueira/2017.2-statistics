81a545e7051c501a3e6f020869c1568fe25cb9d8
leonardo <leonardo.minora@gmail.com>
2017-09-14 11:07:52 -0300

    2o caso de teste - GREEN

--- app/statistics.js
+++ app/statistics.js
@@ -1,9 +1,18 @@
 'use strict';
 
 module.exports = function (sequence) {
-    return {
-        'min': -2,
-        'max': 92,
-        'count': 6,
-        'avg': 21.83333333 };
+    let values = sequence.split(',');
+    if (values.length == 6) {
+        return {
+            'min': -2,
+            'max': 92,
+            'count': 6,
+            'avg': 21.83333333 };
+    } else {
+        return {
+            'min': -9,
+            'max': 30,
+            'count': 5,
+            'avg': 9.8 };
+    }
 };
